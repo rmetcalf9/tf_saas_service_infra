6073d850882de06691f1c30e612b55849e637da7
Robert Metcalf <rmetcalf9@googlemail.com>
2021-03-21 09:47:17 +0000

    fix

--- major_version/output.tf
+++ major_version/output.tf
@@ -1,5 +1,5 @@
 # output.tf
 
 output "upstream" {
-  value = (var.include_main_public || var.include_main_private) ? kong_upstream.upstream : null
+  value = (var.include_main_public || var.include_main_private) ? kong_upstream.upstream["0"] : null
 }
